6479f109647397c9b8df3ca4f8c78bc324324e81
Edwin Mghendi <ediiethan16gmail.com>
2025-10-27 22:43:42 +0300

    created some files and wrote some lines of code

--- nod.js
+++ nod.js
@@ -7,3 +7,66 @@ console.log(`Cost of food is: $${cost}`);
 let cartQuantity = 0;
 //calculator
 let calculation;
+function getNo(){
+let randNO =Math.random();
+let reponse="";
+    if(randNO >= 0 && randNO < 0.33 ){
+        reponse +="Rock";
+    }else if(randNO > 0.33 && randNO < 0.66){
+      reponse +="Paper";
+    }else{
+      reponse +="Scissors";
+    }
+    let giveAward="";
+    if(reponse === "Rock"){
+    giveAward +="A Win!";
+    }else if(reponse === "Paper"){
+    giveAward +="Paper beats Rock";
+    }else{
+    giveAward +="Scissors beats Paper";
+    }
+    document.getElementById("show").innerText=`You selected ${reponse} meaning ${giveAward}`;
+    console.log(`You selected ${reponse} meaning ${giveAward}`);
+}
+function getNo1(){
+  let randNO =Math.random();
+let reponse="";
+    if(randNO >= 0 && randNO < 0.33 ){
+        reponse +="Rock";
+    }else if(randNO > 0.33 && randNO < 0.66){
+      reponse +="Paper";
+    }else{
+      reponse +="Scissors";
+    }
+    let giveAward="";
+    if(reponse === "Rock"){
+    giveAward +="Rock beats Scissors";
+    }else if(reponse === "Paper"){
+    giveAward +="Paper Win!";
+    }else{
+    giveAward +="Scissors beats Paper";
+    }
+    document.getElementById("show").innerText=`You selected ${reponse} meaning ${giveAward}`;
+    console.log(`You selected ${reponse} meaning ${giveAward}`);
+}
+function getNo2(){
+  let randNO =Math.random();
+let reponse="";
+    if(randNO >= 0 && randNO < 0.33 ){
+        reponse +="Rock";
+    }else if(randNO > 0.33 && randNO < 0.66){
+      reponse +="Paper";
+    }else{
+      reponse +="Scissors";
+    }
+    let giveAward="";
+    if(reponse === "Rock"){
+    giveAward +="Rock beats Scissors";
+    }else if(reponse === "Paper"){
+    giveAward +="Paper beats Rock";
+    }else{
+    giveAward +="Scissors Win!";
+    }
+    document.getElementById("show").innerText=`You selected ${reponse} meaning ${giveAward}`;
+    console.log(`You selected ${reponse} meaning ${giveAward}`);
+}
---
+++ week9.js
@@ -0,0 +1,10 @@
+//change background color of the body
+function changeBcolor1(){
+  document.getElementById("parent").style.backgroundColor="red";
+}
+function changeBcolor2(){
+  document.getElementById("parent").style.backgroundColor="blue";
+}
+function changeBcolor3(){
+  document.getElementById("parent").style.backgroundColor="green";
+}
